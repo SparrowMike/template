98556bc560e61e2bb8e030f5e9f57e1a8b46be9c
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-08-26 23:56:52 +0800

    feat: 🎸 basic backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import './../css/style.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const callMe = () => {
+    fetch('http://localhost:5000/api/data', { method: 'GET' })
+        .then(data => data.json())
+        .then(json => alert(JSON.stringify(json)))
+  }
   return (
     <div className="App">
-      
+       <header className="btn-container">
+          <button onClick={callMe}>Call API</button>
+        </header>
     </div>
   )
 }
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,35 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+const bodyParser = require('body-parser');
+const cors = require('cors');
+import mongoose from 'mongoose'
+const userModel = require("./../models/user.js");
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('hey');
+require("dotenv").config({ path: "./config.env" });
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+const url = 'mongodb://localhost:27017/test';
+
+mongoose.connect(url);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
 });
 
-app.listen(5000, () => console.log('Server running'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/api/data', async (req: Request, res: Response, next: NextFunction) => {
+    // const user = await userModel.findOne({id: 123});
+    const user = await userModel.find({});
+    res.send(user);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
